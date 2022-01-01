ba57189b59c702ff9ce95f2554e526ca8557ccf3
nandra <adinandra.dharmasurya@gmail.com>
2022-01-02 04:00:50 +0700

    update code

--- src/main/java/id/codefun/common/model/request/FindByIdRequest.java
+++ src/main/java/id/codefun/common/model/request/FindByIdRequest.java
@@ -1,9 +1,15 @@
 package id.codefun.common.model.request;
 
 import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class FindByIdRequest extends BaseRequest {
 
     @ApiModelProperty(value="ID to Find")
--- src/main/java/id/codefun/common/model/response/PageResponse.java
+++ src/main/java/id/codefun/common/model/response/PageResponse.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class PageResponse extends BaseResponse {
+public class PageResponse<T extends BaseResponse> extends BaseResponse {
 
     private transient Page page;
 
